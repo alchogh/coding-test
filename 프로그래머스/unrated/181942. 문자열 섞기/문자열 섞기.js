df0b1f983011a85935c001777dd517b6e480f59f
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-08-21 01:29:46 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,3 @@
 function solution(str1, str2) {
-    let answer = '';
-    for(i=0 ; i<str1.length ; i++){     
-           answer += str1[i] + str2[i]
-    }
-    return answer
+ return  [...str1].map((x, idx)=> x+str2[idx]).join('')
 }
