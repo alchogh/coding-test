0227a72080ee48d9b2a08286eee1ef4e33e64a45
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-09-01 08:18:09 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.45 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,8 +1,4 @@
 function solution(arr, intervals) {
-    const answer = []
-    const [[a,b],[c,d]] = intervals
-    const first = [...arr].slice(a,b+1)
-    const second = [...arr].slice(c,d+1)
-    const sum = answer.push(first, second)
-    return [...answer[0], ...answer[1]]
+    const [[a,b],[c,d]] = intervals   
+    return [...arr.slice(a,b+1), ...arr.slice(c,d+1)]
 }
