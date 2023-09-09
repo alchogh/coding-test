6f2ddfdc1195d96545647ed68447835c578a1f09
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-09-10 03:42:36 +0900

    [unrated] Title: 1로 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+   let count = 0;
+
+  for (let i = 0; i < num_list.length; i++) {
+    let num = num_list[i];
+    while (num !== 1) {
+      if (num % 2 === 0) {
+        num /= 2;
+      } else {
+        num -= 1;
+        num /= 2;
+      }
+      count++;
+    }
+  }
+    return count
+}
