69a962588bd841e361ead9f2625f21d7805ae83f
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-09-10 04:38:25 +0900

    [unrated] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,17 +1,15 @@
 function solution(num_list) {
-   let count = 0;
-
-  for (let i = 0; i < num_list.length; i++) {
-    let num = num_list[i];
-    while (num !== 1) {
-      if (num % 2 === 0) {
-        num /= 2;
-      } else {
-        num -= 1;
-        num /= 2;
-      }
-      count++;
-    }
-  }
+    let count = 0 
+    num_list.forEach((num)=>{
+        while(num!==1){
+            if(num%2===0){
+                num /= 2
+                count +=1
+            } else {
+                num= (num-1)/2
+                count +=1
+            }
+        }
+    })
     return count
 }
