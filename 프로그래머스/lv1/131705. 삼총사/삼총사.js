bc2b146e2005ca480ec99457641d0f06bfc133d0
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-09-26 08:04:34 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -3,7 +3,8 @@ function solution(number) {
     for(let i=0; i<number.length; i++){
         for(let j=i+1; j<number.length; j++){
             for(let k=j+1; k<number.length; k++){
-                if(number[i]+number[j]+number[k]===0) answer +=1
+                const sum = number[i]+number[j]+number[k]===0
+                if(sum) answer +=1
             }
         }
     }
