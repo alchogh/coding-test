7a4f3cc2fd0a67bfdd81356d2fba515649f7612d
Geonoooo <122069802+alchogh@users.noreply.github.com>
2023-12-17 22:21:40 +0900

    [level unrated] Title: 배열 만들기 5, Time: 1.05 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,9 +1,3 @@
 function solution(intStrs, k, s, l) {
-    const result = []
-    intStrs.forEach(a=>{
-        const sliceStr = a.slice(s,s+l)
-        const num = Number(sliceStr)
-        if(num>k) result.push(num)
-    })
-    return result
+    return intStrs.map(v=> +v.slice(s,s+l)).filter(v=> v>k)
 }
