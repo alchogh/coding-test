bf8e34880cb4e79b162f66efa8a72bf5f6b9d8ce
Geonoooo <122069802+alchogh@users.noreply.github.com>
2024-01-03 21:09:06 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -1,12 +1,13 @@
 function solution(n, slicer, num_list) {
+    const [a,b,c] =slicer
     if(n===1){
-    return num_list.slice(0,slicer[1]+1)   
+    return num_list.slice(0,b+1)   
    } else if(n===2){
-    return num_list.slice(slicer[0],num_list.length+1)
+    return num_list.slice(a,num_list.length+1)
    } else if(n===3){
-       return num_list.slice(slicer[0],slicer[1]+1)
+       return num_list.slice(a,b+1)
    } else{
-       const arrangeNum = num_list.slice(slicer[0],slicer[1]+1)
-       return arrangeNum.filter((v,i)=> i%slicer[2]===0)
+       const arrangeNum = num_list.slice(a,b+1)
+       return arrangeNum.filter((v,i)=> i%c===0)
    }
 }
