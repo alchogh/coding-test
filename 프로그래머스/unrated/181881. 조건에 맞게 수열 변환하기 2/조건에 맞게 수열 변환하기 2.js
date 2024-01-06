6720e2a08c315afccbd73f30c1fa694e6ddcff7f
Geonoooo <122069802+alchogh@users.noreply.github.com>
2024-01-06 13:57:04 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -3,13 +3,9 @@ function solution(arr) {
     let prevArr = arr
     while(true){
        const changeArr = prevArr.map(v=>{
-        if(v>=50 && v%2===0){
-            return v/2
-        } else if(v<50 && v%2===1) {
-           return v*2 +1
-        } else {
-            return v
-        }
+        if(v>=50 && v%2===0) return v/2
+        if(v<50 && v%2===1)  return v*2 +1
+        return v
     }) 
     const isAllSame = prevArr.every((a, i) => a === changeArr[i])
         if(isAllSame) break
