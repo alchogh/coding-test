060fcff650c2f509d7e03a5b8a65f4902d69a18a
Geonoooo <122069802+alchogh@users.noreply.github.com>
2024-01-07 00:10:59 +0900

    [level 1] Title: 약수의 합, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,7 +1,5 @@
 function solution(n) {
-   let answer =0 
-   for(let i=1 ; i<=n ; i++){
-      if(n%i===0)  answer +=i
-   }
-    return answer
+    const arr = Array(n).fill(1).map((v,i)=>v+i)
+    const filterArr = arr.filter(v=> n%v===0)
+    return filterArr.reduce((a,c)=>a+c,0)
 }
