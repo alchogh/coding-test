c2bdc998356dd0777202fa416597f23ef06e0a76
Geonoooo <122069802+alchogh@users.noreply.github.com>
2024-01-16 22:12:08 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,8 @@
+function solution(before, after) {
+    const beforeArr = [...before].sort().join('')
+    const afterArr = [...after].sort().join('')
+    
+
+
+     return beforeArr === afterArr ? 1: 0
+}
