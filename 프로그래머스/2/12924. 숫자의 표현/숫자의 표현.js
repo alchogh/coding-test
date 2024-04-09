3c410d362b3c966a27b56a5a138933a9fc0f55ec
Geonoooo <alchogh01@gmail.com>
2024-04-09 15:49:46 +0900

    [level 2] Title: 숫자의 표현, Time: 1.49 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,34 @@
+function solution(n) {
+    let count = 0; // 연속된 수열을 찾은 경우의 수를 저장할 변수
+    for (let start = 1; start <= n; start++) {
+        let sum = 0;
+        for (let num = start; sum < n; num++) {
+            sum += num; // start에서 시작하여 num을 계속 더해감
+            if (sum == n) {
+                count++;
+                break; 
+            }
+        }
+    }
+    return count;
+}
+
+
+// function solution(n) {
+//     let count = 0;  // 연속된 수열을 찾은 경우의 수를 저장할 변수
+//     let start = 1;  // 시작 숫자
+
+//     while (start <= n) {
+//         let sum = 0;
+//         let num = start;
+//         while (sum < n) {
+//             sum += num; // start에서 시작하여 num을 계속 더해감
+//             num++;      // 다음 숫자로 이동
+//         }
+//         if (sum == n) {
+//             count++; // 합이 n과 같으면 경우의 수 하나 증가
+//         }
+//         start++; // 다음 시작 숫자로 이동
+//     }
+//     return count;
+// }
