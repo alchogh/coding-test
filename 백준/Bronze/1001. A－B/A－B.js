e202a9dc929e5db8ff156bd4526e9ce159164082
Geonoooo <alchogh01@gmail.com>
2025-09-11 10:28:05 +0900

    [Bronze V] Title: A-B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,19 @@
+// 백준 문제 풀이 템플릿
+const fs = require('fs');
+
+// 백준에서는 '/dev/stdin' 경로에서 입력을 받습니다.
+// 로컬에서 테스트할 경우, input.txt 파일을 읽어들입니다.
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+// 파일 전체를 읽어와 문자열로 변환하고, 줄바꿈으로 나눕니다.
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// 이제 input 배열에 문제의 입력값이 줄마다 들어있습니다.
+// 이 아래에 문제 풀이 코드를 작성하면 됩니다.
+
+function solution(data) {
+  const [a, b] = data[0].split(' ').map(v => Number(v));
+  return a - b;
+}
+
+console.log(solution(input));
