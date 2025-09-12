3545c307c7fab0e3602968efd775aca46a2e25cf
Geonoooo <alchogh01@gmail.com>
2025-09-12 11:14:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,16 @@
+// 백준 문제 풀이 템플릿
+const fs = require('fs');
+
+// 백준에서는 '/dev/stdin' 경로에서 입력을 받습니다.
+// 로컬에서 테스트할 경우, input.txt 파일을 읽어들입니다.
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+// 파일 전체를 읽어와 문자열로 변환하고, 줄바꿈으로 나눕니다.
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(year) {
+  const adYear = Number(year) - 543;
+  return adYear;
+}
+
+console.log(solution(input));
