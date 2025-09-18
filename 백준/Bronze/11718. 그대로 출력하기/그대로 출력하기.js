62f2d7421355b077995ade5927e39184924de74a
Geonoooo <alchogh01@gmail.com>
2025-09-18 09:37:11 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,10 @@
+// 백준 문제 풀이 템플릿
+const fs = require('fs');
+
+// 백준에서는 '/dev/stdin' 경로에서 입력을 받습니다.
+// 로컬에서 테스트할 경우, input.txt 파일을 읽어들입니다.
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(input);
