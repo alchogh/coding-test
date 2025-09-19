9e04f8b9e1dc6ae2ae9bf5b5ac84afca164d5d11
Geonoooo <alchogh01@gmail.com>
2025-09-20 01:02:00 +0900

    [Bronze I] Title: 단어 공부, Time: 228 ms, Memory: 16536 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -7,25 +7,23 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 // 파일 전체를 읽어와 문자열로 변환하고, 줄바꿈으로 나눕니다.
 const input = fs.readFileSync(filePath).toString().trim().toUpperCase();
-const arr = input.split('');
-const map = new Map();
 
-for (const ele of arr) {
-  map.set(ele, (map.get(ele) || 0) + 1);
+const charCount = {};
+
+for (const char of input) {
+  charCount[char] = (charCount[char] || 0) + 1;
 }
 
 let maxCount = 0;
 let maxChar = '?';
 
-// 3. Map을 순회하며 가장 많이 나온 알파벳 찾기
-for (let [char, count] of map) {
+for (char in charCount) {
+  const count = charCount[char];
   if (count > maxCount) {
-    maxCount = count;
     maxChar = char;
+    maxCount = count;
   } else if (count === maxCount) {
-    // 가장 많이 사용된 알파벳이 여러 개일 경우 '?'로 변경
     maxChar = '?';
   }
 }
-
 console.log(maxChar);
