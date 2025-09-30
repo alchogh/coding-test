30ce365e4dc9c24fe87873ff294b98691eb6e4db
Geonoooo <alchogh01@gmail.com>
2025-09-30 12:36:24 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 92 ms, Memory: 9480 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -0,0 +1,24 @@
+// 백준 문제 풀이 템플릿
+const fs = require('fs');
+
+// 백준에서는 '/dev/stdin' 경로에서 입력을 받습니다.
+// 로컬에서 테스트할 경우, input.txt 파일을 읽어들입니다.
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+// 파일 전체를 읽어와 문자열로 변환하고, 줄바꿈으로 나눕니다.
+const input = fs.readFileSync(filePath).toString().trim();
+const N = Number(input);
+const result = [];
+for (let i = 1; i <= N; i++) {
+  const spaces = ' '.repeat(N - i);
+  const stars = '*'.repeat(2 * i - 1);
+  result.push(spaces + stars);
+}
+
+for (let i = N - 1; i >= 1; i--) {
+  const spaces = ' '.repeat(N - i);
+  const stars = '*'.repeat(2 * i - 1);
+  result.push(spaces + stars);
+}
+
+console.log(result.join('\n'));
