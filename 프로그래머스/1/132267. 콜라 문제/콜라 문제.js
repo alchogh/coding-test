a6bd351916e90d97ec984647f014fb37253fabb7
Geonoooo <alchogh01@gmail.com>
2026-01-26 15:27:35 +0900

    [level 1] Title: 콜라 문제, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,12 @@
+function solution(a, b, n) {
+    let totalCoke = 0;
+    
+    while(n>=a){
+        const newCoke = Math.floor(n / a) * b
+        
+        totalCoke += newCoke
+        
+        n = (n % a) + newCoke
+    }
+    return totalCoke
+}
